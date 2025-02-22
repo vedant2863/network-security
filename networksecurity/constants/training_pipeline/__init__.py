c95e0511711882e29f32fd0bee05707f7dc888fb
vedant2863 <jvedant727@gmail.com>
2025-02-22 12:33:47 +0530

    Data ingestion components

---
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -0,0 +1,21 @@
+
+"""
+Defining common Constants variablefor training pipeline
+"""
+TARGET_COLUMN= "Result"
+PIPELINE_NAME :str= "NetworkSecurity"
+ARTIFACT_DIR:str = "artifacts"
+FILE_NAME:str ="NetworkData.csv"
+
+TRAIN_FILE_NAME:str = "train.csv"
+TEST_FILE_NAME:str = "test.csv"
+
+"""
+Data Ingestionrelated constants start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME:str = "NetworkData"
+DATA_INGESTION_DATABASE_NAME:str = "deku-ai"
+DATA_INGESTION_DIR_NAME:str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR:str = "feature_store"
+DATA_INGESTION_INGESTED_DIR:str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION:float = 0.2
